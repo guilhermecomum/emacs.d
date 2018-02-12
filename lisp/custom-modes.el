00df6a679cf2bb8a10e71e3e46ceb622ef30c301
Guilherme Guerra <guerra@todocartoes.com.br>
2018-02-12 13:20:32 -0200

    Adds prettier-js

--- lisp/custom-modes.el
+++ lisp/custom-modes.el
@@ -9,6 +9,7 @@
 (require 'web-mode)
 (require 'yaml-mode)
 (require 'rjsx-mode)
+(require 'prettier-js)
 
 (defun custom-modes-yaml ()
   "Configuration for yaml-mode."
@@ -18,6 +19,14 @@
 
 (defun custom-modes-web ()
   "Configuration for web-mode."
+
+  (defun enable-minor-mode (my-pair)
+  ;;; https://github.com/prettier/prettier-emacs
+    "Enable minor mode if filename match the regexp.  MY-PAIR is a cons cell (regexp . minor-mode)."
+    (if (buffer-file-name)
+        (if (string-match (car my-pair) buffer-file-name)
+            (funcall (cdr my-pair)))))
+
   (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
