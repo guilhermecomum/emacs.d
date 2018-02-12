3bae6bad306d6e4a430fdd39808486f7bfc18c36
Guilherme Guerra <guerra@todocartoes.com.br>
2018-02-12 13:20:03 -0200

    Open neotree on project root

--- lisp/custom-general.el
+++ lisp/custom-general.el
@@ -175,7 +175,19 @@
   (setq company-idle-delay .3))
 
 (defun custom-general-neotree()
-  (global-set-key [f8] 'neotree-toggle)
+  (defun neotree-project-dir ()
+    "Open NeoTree using the git root."
+    (interactive)
+    (let ((project-dir (projectile-project-root))
+          (file-name (buffer-file-name)))
+      (neotree-toggle)
+      (if project-dir
+          (if (neo-global--window-exists-p)
+              (progn
+                (neotree-dir project-dir)
+                (neotree-find file-name)))
+        (message "Could not find git project root."))))
+  (global-set-key [f8] 'neotree-project-dir)
   (setq neo-smart-open t))
 
 (defun custom-general-lastpass()
