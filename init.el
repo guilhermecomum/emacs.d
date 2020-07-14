c13d4adc23d566d22b20dce0db8aae0512a01ba7
Guilherme Guerra <guilherme.ga@gmail.com>
2020-07-13 22:43:59 -0300

    A new beginning

--- init.el
+++ init.el
@@ -1,5 +1,22 @@
 ;;; init.el --- My Emacs Setup
 ;;
+;; Author: Guilherme Guerra <guilherme.ga@gmail.com>
+;;
+;; Copyright (C) 2012-2020  Guilherme Guerra
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;
 ;;; Commentary:
 ;;
 ;; Entry point for my Emacs setup.  This module's main goal is to load
@@ -10,29 +27,15 @@
 ;; load path so that configs from lisp folder can be required
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
-;; Load packages
-(require 'cask "~/.emacs.d/.cask/cask/cask.el")
-(cask-initialize)
-
-(setq package-enable-at-startup nil)
+;; Add `melpa` to `package-archives`.
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Load all the fun modules
-(require 'custom-general)
-(require 'custom-mac)
-(require 'custom-editing)
-(require 'custom-modes)
-(require 'custom-theme)
-
-;; Initialize all the modules loaded above
-(custom-general)
-(custom-mac)
-(custom-editing)
-(custom-modes)
-(custom-theme)
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 ;; file with custom-set-variables
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
-
-;;; init.el ends here
