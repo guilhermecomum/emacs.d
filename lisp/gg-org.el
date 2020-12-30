c6453cfa174353d5e9fe9d748c36d399ef70f309
Guilherme Guerra <guilherme.ga@gmail.com>
2020-12-30 10:33:05 -0300

    New org-mode setup
    - Integrating google calendar
    - Using org-journal

--- lisp/gg-org.el
+++ lisp/gg-org.el
@@ -26,12 +26,12 @@
 (defun gg/org/init ()
   "Initial 'org-mode' configuration."
   (add-hook 'org-mode-hook 'turn-on-flyspell)
-  (global-set-key (kbd "C-c i")
-                  (lambda () (interactive) (find-file "~/org/index.org")))
-
-  (use-package org-bullets
-    :config
-    (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))))
+  (global-set-key (kbd "C-c j") 'org-journal-open-current-journal-file)
+  (global-set-key (kbd "C-c a")
+                  (lambda ()
+                    (interactive)
+                    (org-agenda nil "c")))
+  (global-set-key (kbd "C-c /") 'org-capture))
 
 (defun gg/org/misc ()
   "General org stuffs."
@@ -42,23 +42,18 @@
   (global-set-key (kbd "C-c C-x C v")
                   'do-org-show-all-inline-images)
 
+  (setq org-agenda-custom-commands
+        '(("c" "Simple agenda view"
+           ((agenda "")
+            (alltodo "")))))
   (use-package ox-clip
     :config
     (global-set-key (kbd "s-c")
-                  'ox-clip-formatted-copy))
+                    'ox-clip-formatted-copy))
 
-  (custom-set-variables '(org-bullets-bullet-list (quote ("*" "*" ))))
-  (setq left-margin-width 2)
-  (setq right-margin-width 2)
-  (setq org-startup-indented t
-        org-pretty-entities t
-        org-hide-emphasis-markers t
-        org-agenda-block-separator ""
-        org-fontify-whole-heading-line t
-        org-fontify-done-headline t
-        org-fontify-quote-and-verse-blocks t
-        org-catch-invisible-edits 'show-and-error
-        org-cycle-separator-lines 0)
+  ;;Clean bullets
+  (setq org-indent-indentation-per-level 1)
+  (setq org-hide-leading-stars 't)
 
   ;;Setup olivetti mode
   (use-package olivetti
@@ -66,23 +61,55 @@
     (add-hook 'markdown-mode-hook (lambda () (olivetti-mode)))
     (add-hook 'org-mode-hook (lambda () (olivetti-mode)))))
 
-(defun gg/org/utf-8-bullet ()
-  "Replace asterisk chars with sexy UTF-8 Bullets."
-  (font-lock-add-keywords
-   'org-mode
-   '(("^ +\\([-*]\\) "
-      (0 (prog1 ()
-           (compose-region (match-beginning 1) (match-end 1) "•")))))))
+(defun gg/org/journal ()
+  "Setup org-journal."
+  (use-package org-journal
+    :config
+    (setq org-journal-enable-agenda-integration t)
+    (setq org-journal-dir "/gdrive:guilherme.ga@gmail.com:/org/journal/")
+    (setq org-journal-file-type 'yearly)
+    (setq org-journal-time-format "")
+    (setq org-journal-date-format "%A, %d %B %Y"))
 
-;; (defun gg/org/drill ()
-;;   "Install and setup org-drill."
-;;   (use-package org-drill))
+  (defun org-journal-find-location ()
+    ;; Open today's journal, but specify a non-nil prefix argument in order to
+    ;; inhibit inserting the heading; org-capture will insert the heading.
+    (org-journal-new-entry t)
+    (unless (eq org-journal-file-type 'daily)
+      (org-narrow-to-subtree))
+    (goto-char (point-max)))
+
+  (setq org-capture-templates '(("j" "Journal entry" plain (function org-journal-find-location)
+                                 "** %(format-time-string org-journal-time-format)%^{Title}\n%i%?"
+                                 :jump-to-captured t :immediate-finish t))))
+
+(defun gg/org/gcal ()
+  "Setup google calendar."
+  (global-set-key (kbd "C-c C-o c") 'cfw:open-org-calendar)
+  (use-package calfw-org)
+  (use-package org-gcal
+    :init
+    (start-lastpass)
+    (setq org-gcal-client-id (lastpass-getfield "org-gcal-client-id" "ilegra-google"))
+    (setq org-gcal-client-secret (lastpass-getfield "org-gcal-client-secret" "ilegra-google"))
+
+    :config
+    (setq org-gcal-recurring-events-mode 'nested)
+
+    ;; Once that file is created open it C-x C-f </path/to/file> ` and then `M-x org-agenda-file-to-front`. This command will include the new file.
+    ;; source: https://kabads.monkeez.org/sysadmin/emacs/2018/03/18/sync-google-calendar-with-org-agenda.html
+    (setq org-gcal-fetch-file-alist '(("guilherme.guerra@ilegra.com" .  "~/org/calendar.org"))))
+  (setq org-agenda-prefix-format '((agenda  . "  • ")))
+  (add-hook 'org-agenda-mode-hook (lambda () (org-gcal-fetch))))
+
+
 
 (defun gg/org ()
   "Call out other org customization functions."
   (gg/org/init)
   (gg/org/misc)
-  (gg/org/utf-8-bullet))
+  (gg/org/gcal)
+  (gg/org/journal))
 
 (provide 'gg-org)
 
