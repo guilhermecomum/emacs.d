45830b77655b7b87255c3f8759dc887ec2f3b3cd
Guilherme Guerra <guilherme.ga@gmail.com>
2022-02-25 18:08:29 -0300

    wip migrating config to literate configuration

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(js-indent-level 2)
  '(org-agenda-files '("/home/milesdavis/Projects/org-files/journal/2022.org"))
  '(package-selected-packages
-   '(csv-mode ox-slack typescript-mode pyvenv with-venv yasnippet web-mode use-package svelte-mode smartparens sass-mode robe rjsx-mode restclient rainbow-delimiters prettier-js pocket-reader pass ox-reveal ox-clip org-super-agenda org-journal org-contrib olivetti nyan-mode neotree multiple-cursors move-text markdown-mode magit lorem-ipsum js-comint jedi helm-projectile helm-ag hackernews gradle-mode google-translate go-mode git-auto-commit-mode fortune-cookie flyspell-correct-popup flycheck exec-path-from-shell emmet-mode eglot editorconfig doom-themes doom-modeline docker-compose-mode dimmer company-box auto-rename-tag auto-package-update all-the-icons-dired add-node-modules-path))
+   '(yafolding restclient nyan-mode doom-themes all-the-icons-dired all-the-icons auto-package-update use-package))
  '(sgml-basic-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
