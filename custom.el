80fc4caa19ef4d0dc615f2c516db5b007c51ac7c
Guilherme Guerra <guilherme.ga@gmail.com>
2022-02-27 10:19:57 -0300

    Finish config migration

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(prettier-js tide rjsx-mode org-super-agenda org-journal olivetti htmlize ox-reveal ox-slack git-auto-commit-mode org-contrib helm-ag helm-projectile restclient yafolding yasnippet flycheck add-node-modules-path multiple-cursors company-box company rainbow-delimiters smartparens doom-modeline dimmer nyan-mode auto-package-update use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,1 @@
+(org-babel-load-file "~/.emacs.d/configuration.org")
