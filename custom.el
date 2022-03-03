df33eb12c257728813b67ea0a8b540254284ebb8
Guilherme Guerra <guilherme.ga@gmail.com>
2022-03-02 23:37:03 -0300

    Better org-mode UI with olivetti

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    '(prettier-js tide rjsx-mode org-super-agenda org-journal olivetti htmlize ox-reveal ox-slack git-auto-commit-mode org-contrib helm-ag helm-projectile restclient yafolding yasnippet flycheck add-node-modules-path multiple-cursors company-box company rainbow-delimiters smartparens doom-modeline dimmer nyan-mode auto-package-update use-package)))
+ '(olivetti-body-width 105)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
