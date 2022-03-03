f63abbfb8056b1dcfb08a2eb78ea03d1eea10ba8
Guilherme Guerra <guilherme.ga@gmail.com>
2022-03-02 23:37:39 -0300

    Define org-agenda file

--- custom.el
+++ custom.el
@@ -3,9 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(prettier-js tide rjsx-mode org-super-agenda org-journal olivetti htmlize ox-reveal ox-slack git-auto-commit-mode org-contrib helm-ag helm-projectile restclient yafolding yasnippet flycheck add-node-modules-path multiple-cursors company-box company rainbow-delimiters smartparens doom-modeline dimmer nyan-mode auto-package-update use-package)))
  '(olivetti-body-width 105)
+ '(org-agenda-files '("/home/milesdavis/Projects/org-files/journal/2022.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
