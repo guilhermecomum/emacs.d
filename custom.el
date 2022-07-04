a74817568eecb0d7b3994f87b4cc374186f28f01
Guilherme Guerra <guilherme.ga@gmail.com>
2022-07-04 17:51:13 -0300

    Small improvements

--- custom.el
+++ custom.el
@@ -3,8 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(olivetti-body-width 105)
- '(org-agenda-files '("/home/guerra/Projects/org-files/journal/2022.org")))
+ '(org-agenda-files '("/home/guerra/Projects/org-files/journal/2022.org"))
+ '(safe-local-variable-values
+   '((eval progn
+           (require 'projectile)
+           (puthash
+            (projectile-project-root)
+            "cask exec buttercup -L ." projectile-test-cmd-map)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
