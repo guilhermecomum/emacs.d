ae9a56e3e64f695f1d50e246f7d52d9c9f352c77
Guilherme Guerra <guilherme.ga@gmail.com>
2024-12-04 16:30:39 -0300

    Add android early-init

---
+++ early-init.el
@@ -0,0 +1,5 @@
+(when (string-equal system-type "android")
+  ;; Add Termux binaries to PATH environment
+  (let ((termuxpath "/data/data/com.termux/files/usr/bin"))
+    (setenv "PATH" (concat (getenv "PATH") ":" termuxpath))
+    (setq exec-path (append exec-path (list termuxpath)))))
