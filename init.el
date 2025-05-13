7c79078e69042fb14086f9e8a41ed395723a2d8c
Guilherme Guerra <guilherme.ga@gmail.com>
2025-05-13 15:01:40 -0300

    Takes back transient files

--- init.el
+++ init.el
@@ -1,1 +1,13 @@
 (org-babel-load-file "~/.emacs.d/new.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values '((projectile-project-compilation-cmd . "tsc"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
