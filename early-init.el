a2e645ddd01d611420a7f75a557773572b2e4fd7
Guilherme Guerra <guilherme.ga@gmail.com>
2025-12-24 11:52:39 -0300

    Fix initialization

--- early-init.el
+++ early-init.el
@@ -3,7 +3,7 @@
 ;; Adjust garbage collection for faster startup
 (setq gc-cons-threshold (* 50 1000 1000))
 
-;; Disable package.el in favor of straight.el
+;; Disable package.el in favor of Elpaca
 (setq package-enable-at-startup nil)
 
 ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early
@@ -13,5 +13,9 @@
 
 ;; Prevent unwanted runtime builds in gccemacs; packages are compiled ahead of time
 (setq comp-deferred-compilation nil)
+
+;; Load a built-in theme early to avoid flash of unstyled Emacs
+;; This will be replaced by doom-themes later in readme.org
+(load-theme 'misterioso t)
 
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -1,2 +1,3 @@
-(load-theme 'misterioso)
+;; Don't load theme here - it triggers package.el before Elpaca initializes
+;; Theme is configured in readme.org via doom-themes
 (org-babel-load-file "~/.emacs.d/readme.org")
