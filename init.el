9bc3e580f3473ad7db8dfc5627e4336ced9f4cfd
Guilherme Guerra <guilherme.ga@gmail.com>
2026-04-15 15:33:04 -0300

    Removes the mcp code from init.el

--- init.el
+++ init.el
@@ -1,7 +1,3 @@
 ;; Don't load theme here - it triggers package.el before Elpaca initializes
 ;; Theme is configured in readme.org via doom-themes
-
-(add-to-list 'load-path "~/Projects/emacs-mcp/")
-(require 'emacs-mcp)
-
 (org-babel-load-file "~/.emacs.d/readme.org")
