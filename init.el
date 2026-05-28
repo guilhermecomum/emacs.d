3b01557fac708af8e9a9571a5835a91d785b12de
Guilherme Guerra <guilherme.ga@gmail.com>
2026-05-28 11:49:30 -0300

    Adds csv-highlight

--- init.el
+++ init.el
@@ -1177,6 +1177,23 @@ should be checked."
 
 (use-package csv-mode :ensure t)
 
+(defun csv-highlight (&optional separator)
+  (interactive (list (when current-prefix-arg (read-char "Separator: "))))
+  (font-lock-mode 1)
+  (let* ((separator (or separator ?\,))
+         (n (count-matches (string separator) (pos-bol) (pos-eol)))
+         (colors (cl-loop for i from 0 to 1.0 by (/ 2.0 n)
+                          collect (apply #'color-rgb-to-hex
+                                         (color-hsl-to-rgb i 0.3 0.5)))))
+    (cl-loop for i from 2 to n by 2
+             for c in colors
+             for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
+             do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
+
+(add-hook 'csv-mode-hook 'csv-align-mode)
+(add-hook 'csv-mode-hook '(lambda () (interactive) (toggle-truncate-lines nil)))
+
+
 ;;; Compile
 
 (use-package compile
